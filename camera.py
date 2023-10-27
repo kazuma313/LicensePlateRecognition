a209ee892f2408884677138574c821b94ea0147b
kazuma313 <koerzoelmatondang@gmail.com>
2023-10-28 06:20:59 +0700

    memperbaiki camera.py dan menambah file camera_easyOCR.py untuk modul easyOCR

--- camera.py
+++ camera.py
@@ -1,67 +1,71 @@
 import cv2
-import model as mn
-# import yolov5
-import numpy as np
 
-model_character = mn.model_character()
-model_detection = mn.model_platDetection(model_path="skiba4/license_plate")
-
-cam = cv2.VideoCapture(0)
-cv2.namedWindow('test')
-
-img_counter = 0
-fileName_detect_plate = "deteksi_plat.jpg"
-
-while True:
-    ret,frame = cam.read()
+def camera_forDetection(model_character, model_detection):
     
-    if not ret:
-        print("failed to grab frame")
-        break
-    
-    img = cv2.imwrite(fileName_detect_plate, frame)
-    
-    deteksi_plat = model_detection.predict(fileName_detect_plate)
+    cam = cv2.VideoCapture(0)
+    cv2.namedWindow('camera')
 
-    if deteksi_plat:
-        start_point = (deteksi_plat[0]['box']['xmin'], deteksi_plat[0]['box']['ymin'])
-        end_point = (deteksi_plat[0]['box']['xmax'], deteksi_plat[0]['box']['ymax'])
-        nomor_plat = "".join(model_character.predict_image(fileName_detect_plate))
+    img_counter = 0
+    fileName_detect_plate = "deteksi_plat.jpg"
+
+    while True:
+        ret,frame = cam.read()
         
-        # put text to image
-        cv2.putText(img=frame, 
-                    text=nomor_plat,
-                    org= start_point,
-                    fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                    fontScale=1,
-                    color=(0, 0, 200),
-                    thickness=2, 
-                    )
+        if not ret:
+            print("failed to grab frame")
+            break
         
-        # put rectangle to image
-        cv2.rectangle(frame,
-                      start_point, 
-                      end_point, 
-                      (0, 255, 0), 
-                      5
-                      )
+        img = cv2.imwrite(fileName_detect_plate, frame)
         
-    img = cv2.imwrite("plat_detected.jpg", frame)
-    
-    # Menampilkan image
-    cv2.imshow('test', frame)
-    
-    k = cv2.waitKey(1)
-    
-    if k % 256 == 27:
-        print("Closing app")
-        break
-    
-    elif k % 256 ==32:
-        img_name = "open_Cv_{}.jpg".format(img_counter)
-        cv2.imwrite(img_name, frame)
-        print("ScreenShoot")
-        img_counter += 1
+        deteksi_plat = model_detection.predict(fileName_detect_plate)
 
+        if deteksi_plat:
+            start_point = (deteksi_plat[0]['box']['xmin'], deteksi_plat[0]['box']['ymin'])
+            end_point = (deteksi_plat[0]['box']['xmax'], deteksi_plat[0]['box']['ymax'])
+            nomor_plat = "".join(model_character.predict_image(fileName_detect_plate))
+            
+            # put text to image
+            cv2.putText(img=frame, 
+                        text=nomor_plat,
+                        org= start_point,
+                        fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                        fontScale=1,
+                        color=(0, 0, 200),
+                        thickness=2, 
+                        )
+            
+            # put rectangle to image
+            cv2.rectangle(frame,
+                        start_point, 
+                        end_point, 
+                        (0, 255, 0), 
+                        5
+                        )
+            
+        img = cv2.imwrite("plat_detected.jpg", frame)
         
-cam.release()
+        # Menampilkan image
+        cv2.imshow('camera', frame)
+        
+        k = cv2.waitKey(1)
+        
+        if k % 256 == 27:
+            print("Closing app")
+            break
+        
+        elif k % 256 ==32:
+            img_name = "open_Cv_{}.jpg".format(img_counter)
+            cv2.imwrite(img_name, frame)
+            print("ScreenShoot")
+            img_counter += 1
+
+            
+    cam.release()
+
+
+if __name__ == "__main__": 
+    import model as mn
+    model_character = mn.model_character()
+    model_detection = mn.model_platDetection(model_path="skiba4/license_plate")
+    camera_forDetection(model_character=model_character, model_detection=model_detection)
+    
---
+++ camera_easyOCR.py
@@ -0,0 +1,47 @@
+import cv2
+import easyocr
+reader = easyocr.Reader(['en'])
+
+cam = cv2.VideoCapture(0)
+cv2.namedWindow('test')
+
+img_counter = 0
+
+while True:
+    ret,frame = cam.read()
+    
+    if not ret:
+        print("failed to grab frame")
+        break
+    
+    result = reader.readtext(frame)
+
+        # put text to image
+    if result:
+        cv2.putText(img=frame, 
+                    text= " ".join([i[-2] for i in result[:4]]),
+                    org= (50, 50),
+                    fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                    fontScale=1,
+                    color=(0, 0, 200),
+                    thickness=2, 
+                    )
+        
+    
+    # Menampilkan image
+    cv2.imshow('test', frame)
+    
+    k = cv2.waitKey(1)
+    
+    if k % 256 == 27:
+        print("Closing app")
+        break
+    
+    elif k % 256 ==32:
+        img_name = "open_Cv_{}.jpg".format(img_counter)
+        cv2.imwrite(img_name, frame)
+        print("ScreenShoot")
+        img_counter += 1
+
+        
+cam.release()
